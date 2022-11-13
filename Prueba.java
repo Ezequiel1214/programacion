e304e38c086b994f9d98c9aaa34a0a75919bd2a8
Ezequiel1214 <118034570+Ezequiel1214@users.noreply.github.com>
2022-11-12 21:04:19 -0400

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,39 @@
+
+package prueba;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ezequiel Faria
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+       Scanner lector = new Scanner(System.in);
+String Texto="";
+String caracter="";
+int cont=0;
+
+System.out.println("Ingresa el texto ");
+Texto = lector.nextLine();
+
+System.out.println("¿que caracter deseas contar? ");
+caracter = lector.nextLine();
+for (int i = 0; i < Texto.length(); i++) {
+if(Texto.charAt(i)== caracter.charAt(0) ){
+cont ++;
+}
+}
+
+if (cont != 0) {
+System.out.println("El caracter "+caracter.charAt(0)+ " se repite " + cont + " veces");
+}else{
+System.out.println("El caracter no se encuentra en este texto");
+}
+    }
+}
